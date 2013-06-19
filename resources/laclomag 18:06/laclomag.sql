231d89f2bcb322a260e5983aea4c11752648ace3
Alejandro Pardo <alejandro.pardo.r@gmail.com>
2013-06-18 23:43:11 -0430

    base de datos

--- resources/laclomag 18:06/laclomag.sql
+++ resources/laclomag 18:06/laclomag.sql
@@ -8,6 +8,8 @@ USE `laclomag` ;
 -- -----------------------------------------------------
 -- Table `laclomag`.`users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`users` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(50) NOT NULL ,
@@ -29,6 +31,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `laclomag`.`papers`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`papers` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`papers` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(50) NULL ,
@@ -43,6 +47,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `laclomag`.`magazines`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`magazines` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`magazines` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(50) NULL ,
@@ -59,6 +65,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `laclomag`.`messages`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`messages` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`messages` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `content` VARCHAR(255) NULL ,
@@ -73,6 +81,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `laclomag`.`news`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`news` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`news` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `headline` VARCHAR(255) NULL ,
@@ -94,6 +104,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `laclomag`.`logbooks`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`logbooks` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`logbooks` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` VARCHAR(45) NULL ,
@@ -109,91 +121,103 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `laclomag`.`admins`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`admins` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`admins` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` INT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `user_id_idx` (`user_id` ASC) ,
-  CONSTRAINT `user_id`
+  CONSTRAINT `admins_user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `laclomag`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`authors`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`authors` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`authors` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` INT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `user_id_idx` (`user_id` ASC) ,
-  CONSTRAINT `user_id`
+  CONSTRAINT `authors_user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `laclomag`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`editors`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`editors` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`editors` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` INT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `user_id_idx` (`user_id` ASC) ,
-  CONSTRAINT `user_id`
+  CONSTRAINT `editors_user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `laclomag`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`evaluators`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`evaluators` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`evaluators` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` INT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `user_id_idx` (`user_id` ASC) ,
-  CONSTRAINT `user_id`
+  CONSTRAINT `evaluators_user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `laclomag`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`readers`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`readers` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`readers` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` INT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `user_id_idx` (`user_id` ASC) ,
-  CONSTRAINT `user_id`
+  CONSTRAINT `readers_user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `laclomag`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`reader_comments`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`reader_comments` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`reader_comments` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `magazine_id` INT NULL ,
@@ -205,22 +229,24 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`reader_comments` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `magazine_id_idx` (`magazine_id` ASC) ,
   INDEX `reader_id_idx` (`user_id` ASC) ,
-  CONSTRAINT `reader_id`
+  CONSTRAINT `reader_comments_reader_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `laclomag`.`readers` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `magazine_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `reader_comments_magazine_id`
     FOREIGN KEY (`magazine_id` )
     REFERENCES `laclomag`.`magazines` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`mapped_messages`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`mapped_messages` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`mapped_messages` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `message_id` INT NULL ,
@@ -230,22 +256,24 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`mapped_messages` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `user_id_idx` (`user_id` ASC) ,
   INDEX `message_id_idx` (`message_id` ASC) ,
-  CONSTRAINT `user_id`
+  CONSTRAINT `mapped_messages_user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `laclomag`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `message_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `mapped_messages_message_id`
     FOREIGN KEY (`message_id` )
     REFERENCES `laclomag`.`messages` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`paper_files`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`paper_files` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_files` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `paper_id` INT NULL ,
@@ -256,18 +284,20 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_files` (
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `paper_id_idx` (`paper_id` ASC) ,
-  CONSTRAINT `paper_id`
+  CONSTRAINT `paper_files_paper_id`
     FOREIGN KEY (`paper_id` )
     REFERENCES `laclomag`.`papers` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `laclomag`.`paper_author`
+-- Table `laclomag`.`paper_authors`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_author` (
+DROP TABLE IF EXISTS `laclomag`.`paper_authors` ;
+
+CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_authors` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `paper_id` INT NULL ,
   `author_id` INT NULL ,
@@ -275,23 +305,25 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_author` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `paper_id_idx` (`paper_id` ASC) ,
   INDEX `author_id_idx` (`author_id` ASC) ,
-  CONSTRAINT `paper_id`
+  CONSTRAINT `paper_authors_paper_id`
     FOREIGN KEY (`paper_id` )
     REFERENCES `laclomag`.`papers` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `author_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `paper_authors_author_id`
     FOREIGN KEY (`author_id` )
     REFERENCES `laclomag`.`authors` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `laclomag`.`paper_editor`
+-- Table `laclomag`.`paper_editors`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_editor` (
+DROP TABLE IF EXISTS `laclomag`.`paper_editors` ;
+
+CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_editors` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `paper_id` INT NULL ,
   `editor_id` INT NULL ,
@@ -301,23 +333,25 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_editor` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `paper_id_idx` (`paper_id` ASC) ,
   INDEX `editor_id_idx` (`editor_id` ASC) ,
-  CONSTRAINT `paper_id`
+  CONSTRAINT `paper_editors_paper_id`
     FOREIGN KEY (`paper_id` )
     REFERENCES `laclomag`.`papers` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `editor_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `paper_editors_editor_id`
     FOREIGN KEY (`editor_id` )
     REFERENCES `laclomag`.`editors` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `laclomag`.`paper_evaluator`
+-- Table `laclomag`.`paper_evaluators`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_evaluator` (
+DROP TABLE IF EXISTS `laclomag`.`paper_evaluators` ;
+
+CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_evaluators` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `paper_id` INT NULL ,
   `evaluator_id` INT NULL ,
@@ -327,22 +361,24 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_evaluator` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `paper_id_idx` (`paper_id` ASC) ,
   INDEX `evaluator_id_idx` (`evaluator_id` ASC) ,
-  CONSTRAINT `paper_id`
+  CONSTRAINT `paper_evaluators_paper_id`
     FOREIGN KEY (`paper_id` )
     REFERENCES `laclomag`.`papers` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `evaluator_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `paper_evaluators_evaluator_id`
     FOREIGN KEY (`evaluator_id` )
     REFERENCES `laclomag`.`evaluators` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`paper_comments`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`paper_comments` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_comments` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `paper_id` INT NULL ,
@@ -353,23 +389,25 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`paper_comments` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `paper_id_idx` (`paper_id` ASC) ,
   INDEX `evaluator_id_idx` (`evaluator_id` ASC) ,
-  CONSTRAINT `paper_id`
+  CONSTRAINT `paper_comments_paper_id`
     FOREIGN KEY (`paper_id` )
     REFERENCES `laclomag`.`papers` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `evaluator_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `paper_comments_evaluator_id`
     FOREIGN KEY (`evaluator_id` )
     REFERENCES `laclomag`.`evaluators` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `laclomag`.`magazine_paper`
+-- Table `laclomag`.`magazine_papers`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_paper` (
+DROP TABLE IF EXISTS `laclomag`.`magazine_papers` ;
+
+CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_papers` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `magazine_id` INT NULL ,
   `paper_id` INT NULL ,
@@ -377,22 +415,24 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_paper` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `magazine_id_idx` (`magazine_id` ASC) ,
   INDEX `paper_id_idx` (`paper_id` ASC) ,
-  CONSTRAINT `magazine_id`
+  CONSTRAINT `magazine_papers_magazine_id`
     FOREIGN KEY (`magazine_id` )
     REFERENCES `laclomag`.`magazines` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `paper_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `magazine_papers_paper_id`
     FOREIGN KEY (`paper_id` )
     REFERENCES `laclomag`.`papers` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `laclomag`.`magazine_files`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `laclomag`.`magazine_files` ;
+
 CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_files` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `magazine_id` INT NULL ,
@@ -402,18 +442,20 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_files` (
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `magazine_id_idx` (`magazine_id` ASC) ,
-  CONSTRAINT `magazine_id`
+  CONSTRAINT `magazine_files_magazine_id`
     FOREIGN KEY (`magazine_id` )
     REFERENCES `laclomag`.`magazines` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `laclomag`.`magazine_editor`
+-- Table `laclomag`.`magazine_editors`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_editor` (
+DROP TABLE IF EXISTS `laclomag`.`magazine_editors` ;
+
+CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_editors` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `magazine_id` INT NULL ,
   `editor_id` INT NULL ,
@@ -422,16 +464,16 @@ CREATE  TABLE IF NOT EXISTS `laclomag`.`magazine_editor` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
   INDEX `magazine_id_idx` (`magazine_id` ASC) ,
   INDEX `editor_id_idx` (`editor_id` ASC) ,
-  CONSTRAINT `magazine_id`
+  CONSTRAINT `magazine_editor_magazine_id`
     FOREIGN KEY (`magazine_id` )
     REFERENCES `laclomag`.`magazines` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `editor_id`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `magazine_editor_editor_id`
     FOREIGN KEY (`editor_id` )
     REFERENCES `laclomag`.`editors` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 USE `laclomag` ;
