2fa007c3e7ce24f435eb540d936c35843fd8bb03
Alejandro Pardo <alejandro.pardo.r@gmail.com>
2013-08-09 18:26:15 -0430

    cleanup del wkhtmltopdf

--- app/Controller/PaperFilesController.php
+++ app/Controller/PaperFilesController.php
@@ -1,7 +1,6 @@
 <?php
 App::uses('AppController', 'Controller');
 App::uses('CakePdf', 'CakePdf.Pdf');
-//require '../Vendor/WkHtmlToPdf.php';
 /**
  * PaperFiles Controller
  *
@@ -28,56 +27,5 @@ class PaperFilesController extends AppController {
 
         $html = $this->PaperFile->findById($id);
         $this->set('htm', $html['PaperFile']['raw']);
-     /*   $CakePdf = new CakePdf();
-	    //$CakePdf->template('newsletter', 'default');
-	    //get the pdf string returned
-	    $pdf = $CakePdf->output();
-	    //or write it to file directly
-	    $pdf = $CakePdf->write(APP . 'files' . DS . 'newsletter.pdf');*/
-        
-
-     
-
-    //this function is required for wkhtmltopdf to retrieve the viewdump once it's rendered 
-    /*public function getViewDump($fileName) 
-    { 
-        $this->WkHtmlToPdf->getViewDump($fileName); 
-    } 
-	/*public function view() {
-		//$this->autoRender = false;
-		$this->layout = 'ajax';
-
-		if (!$this->PaperFile->exists('13')) {
-		  throw new NotFoundException(__('Invalid pdf'));
-		} else {
-			//$this->autoRender = false; // we will handle our own rendering
-
-			$html = $this->PaperFile->findById('13');
-
-			// get an instance of wkhtmltopdf
-			/** OSX **/
-			//$binary = APP . "Vendor/wkhtmltopdf.app/Contents/MacOS/wkhtmltopdf";
-
-			/** LINUX **/
-			//$binary = APP . "Vendor/wkhtmltopdf-amd64";
-
-			/*$pdf = new WkHtmlToPdf(array(
-			  'bin'           => $binary,
-			  'orientation'   => 'landscape'
-			));*/
-
-			// decode the database and add the html to the pdf
-			//$html = $pdf_file['PaperFile']['raw'];
-			//$pdf->addPage($html);
-/*
-			$this->set('htm', $html['PaperFile']['raw']);
-			$this->WkHtmlToPdf->createPdf(); 
-			$this->redirect(array("controller" => "backend", "action" => "index"));
-
-			// send the file to the user's browser without saving it on the server!
-			/*if(!$pdf->send() ){
-			  throw new Exception('Could not create PDF: '.$pdf->getError());
-			}*/
-		/*}*/
 	}
 }
